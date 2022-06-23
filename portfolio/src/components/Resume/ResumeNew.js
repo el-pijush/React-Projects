2d7ec2686ce7b83a0247c4f9a5a69af38ca1e2a9
pijush bera <pijushbera98@gmail.com>
2022-06-23 20:14:13 +0530

    pij

--- portfolio/src/components/Resume/ResumeNew.js
+++ portfolio/src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumyajit_Behera-BIT_MESRA.pdf";
+import pdf from "../../Assets/../Assets/c2.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "";
+  "https://github.com/el-pijush/React-Projects/blob/main/portfolio/src/Assets/c2.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
